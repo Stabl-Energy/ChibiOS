69178cbb5f9c8e72e480bad700b3a0ffe7f82228
Thomas Elste <thomas.elste@imms.de>
2020-09-29 15:08:31 +0200

    Add EFL driver for STM32F7xx
    Add low level flash driver for the STM32F7xx family of controllers
    based on the STM32F4xx driver.

--- os/hal/ports/STM32/STM32F7xx/hal_lld.h
+++ os/hal/ports/STM32/STM32F7xx/hal_lld.h
@@ -984,6 +984,7 @@
 #define STM32_7WS_THRESHOLD         STM32_SYSCLK_MAX
 #define STM32_8WS_THRESHOLD         0
 #define STM32_9WS_THRESHOLD         0
+#define STM32_FLASH_PSIZE           2
 
 #elif (STM32_VDD >= 240) && (STM32_VDD < 270)
 #define STM32_0WS_THRESHOLD         24000000
@@ -996,6 +997,7 @@
 #define STM32_7WS_THRESHOLD         192000000
 #define STM32_8WS_THRESHOLD         STM32_SYSCLK_MAX
 #define STM32_9WS_THRESHOLD         0
+#define STM32_FLASH_PSIZE           1
 
 #elif (STM32_VDD >= 210) && (STM32_VDD < 240)
 #define STM32_0WS_THRESHOLD         22000000
@@ -1008,6 +1010,7 @@
 #define STM32_7WS_THRESHOLD         176000000
 #define STM32_8WS_THRESHOLD         198000000
 #define STM32_9WS_THRESHOLD         STM32_SYSCLK_MAX
+#define STM32_FLASH_PSIZE           1
 
 #elif (STM32_VDD >= 180) && (STM32_VDD < 210)
 #define STM32_0WS_THRESHOLD         20000000
@@ -1020,6 +1023,8 @@
 #define STM32_7WS_THRESHOLD         160000000
 #define STM32_8WS_THRESHOLD         180000000
 #define STM32_9WS_THRESHOLD         0
+#define STM32_FLASH_PSIZE           0
+
 
 #else
 #error "invalid VDD voltage specified"
